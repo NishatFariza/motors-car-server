2fab2aad0f5211bd585563f3b42fe8fd765ac968
NishatFariza <nishatfarzia78@gmail.com>
2022-04-29 17:50:56 +0600

    heroku login add

--- index.js
+++ index.js
@@ -11,8 +11,6 @@ app.use(cors());
 app.use(express.json())
 
 //connect to database
-
-
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.bymrp.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 const motorCollection = client.db("motorHouse").collection("cars");
