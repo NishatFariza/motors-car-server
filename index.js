5dcb3ca7a6e7c75d4ad36e2ee96ed9bd798cc582
NishatFariza <nishatfarzia78@gmail.com>
2022-05-08 14:22:18 +0600

    jwt error handle

--- index.js
+++ index.js
@@ -24,6 +24,7 @@ function verifyJWT(req, res, next){
             console.log(err);
             return res.status(403).send({message: 'Forbidden access'})
         }
+        // console.log('decoded', decoded);
         req.decoded=decoded
         next()
     })
